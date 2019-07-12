9ef0771553e7d55dbd1672cb208418fb6bf884e1
Oleg V. Bobrik <olegvbobrik@gmail.com>
2019-07-12 16:18:48 +0500

    Adaptive blocks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 // Styles
 import './scss/main.scss';
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  let menuButton = document.querySelector(`.page-header__button-menu`);
+  let menu = document.querySelector(`.nav`);
+
+  if (window.innerWidth < 769) {
+    menu.classList.add(`nav--hide`);
+  }
+
+  menuButton.addEventListener(`click`, () => {
+    menu.classList.toggle(`nav--hide`);
+    menuButton.classList.toggle(`page-header__button-menu--open`);
+  });
+
+  window.addEventListener(`resize`, () => {
+    window.innerWidth > 769 ?
+      menu.classList.remove(`nav--hide`) :
+      menu.classList.add(`nav--hide`);
+  });
+});
